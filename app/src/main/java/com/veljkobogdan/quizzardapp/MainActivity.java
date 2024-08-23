104ae7cdc52e16d8784fc185bb582dd2b9d8eed1
VeljkoBogdan <action@dr.com>
2024-08-23 17:49:54 +0200

    minor

--- app/src/main/java/com/veljkobogdan/quizzardapp/MainActivity.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/MainActivity.java
@@ -1,21 +1,11 @@
 package com.veljkobogdan.quizzardapp;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.SurfaceControl;
-import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -41,24 +31,19 @@ public class MainActivity extends AppCompatActivity{
         loadFragment(homeFragment);
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
-            Toast.makeText(this, "tried", Toast.LENGTH_SHORT).show();
             try {
                 int itemId = item.getItemId();
                 if (itemId == R.id.files) {
                     loadFragment(filesFragment);
-                    Toast.makeText(this, "files", Toast.LENGTH_SHORT).show();
                     return true;
                 } else if (itemId == R.id.home) {
                     loadFragment(homeFragment);
-                    Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
                     return true;
                 } else if (itemId == R.id.settings) {
                     loadFragment(settingsFragment);
-                    Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
                     return true;
                 } else if (itemId == R.id.profile) {
                     loadFragment(profileFragment);
-                    Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
                     return true;
                 }
             }
