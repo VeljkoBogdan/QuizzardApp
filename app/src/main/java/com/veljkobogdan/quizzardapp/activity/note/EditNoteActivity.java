7d6607bc3f01b642319b4a086e729c6e09e94ac8
VeljkoBogdan <action@dr.com>
2024-08-25 15:17:31 +0200

    added the ability to edit notes

--- app/src/main/java/com/veljkobogdan/quizzardapp/activity/note/EditNoteActivity.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/activity/note/EditNoteActivity.java
@@ -1,6 +1,10 @@
 package com.veljkobogdan.quizzardapp.activity.note;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +13,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.veljkobogdan.quizzardapp.R;
+import com.veljkobogdan.quizzardapp.database.NoteDAO;
+import com.veljkobogdan.quizzardapp.database.RoomDB;
+import com.veljkobogdan.quizzardapp.entity.Note;
+
+import java.util.Calendar;
 
 public class EditNoteActivity extends AppCompatActivity {
+    ImageView image_submit, image_back;
+    EditText edit_note_title, edit_note_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +33,43 @@ public class EditNoteActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // get Note from intent
+        Note note = (Note) getIntent().getSerializableExtra("note");
+        assert note != null;
+
+        // find views
+        image_back = findViewById(R.id.image_back);
+        image_submit = findViewById(R.id.image_submit);
+        edit_note_text = findViewById(R.id.edit_note_text);
+        edit_note_title = findViewById(R.id.edit_note_title);
+
+        // set click listeners
+        image_back.setOnClickListener(v -> finish());
+        image_submit.setOnClickListener(v -> {
+            String title = edit_note_title.getText().toString();
+            String text = edit_note_text.getText().toString();
+
+            if (title.isEmpty() || text.isEmpty()) {
+                Toast.makeText(this, "All fields are required", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Note newNote = new Note()
+                    .setText(text)
+                    .setTitle(title);
+
+            NoteDAO noteDAO = RoomDB.getInstance(this).noteDAO();
+            noteDAO.update(note.getId(), newNote.getTitle(), newNote.getText(), note.getDate(), note.isPinned);
+
+            Intent intent = new Intent(EditNoteActivity.this, NoteActivity.class);
+            intent.putExtra("note", newNote);
+            startActivity(intent);
+        });
+
+        // set note data
+        edit_note_text.setText(note.getTitle());
+        edit_note_title.setText(note.getText());
     }
 }
