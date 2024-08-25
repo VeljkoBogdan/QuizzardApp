eee74947c8598d69f16f1551e03ca8833908858a
VeljkoBogdan <action@dr.com>
2024-08-25 15:17:07 +0200

    Files Fragment added Notes button

--- app/src/main/java/com/veljkobogdan/quizzardapp/fragments/FilesFragment.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/fragments/FilesFragment.java
@@ -1,5 +1,7 @@
 package com.veljkobogdan.quizzardapp.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +9,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.veljkobogdan.quizzardapp.R;
+import com.veljkobogdan.quizzardapp.activity.note.NotesListActivity;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,15 +22,7 @@ import com.veljkobogdan.quizzardapp.R;
  * create an instance of this fragment.
  */
 public class FilesFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button files_notes_button;
 
     public FilesFragment() {
         // Required empty public constructor
@@ -34,16 +32,12 @@ public class FilesFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment FilesFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static FilesFragment newInstance(String param1, String param2) {
         FilesFragment fragment = new FilesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +45,21 @@ public class FilesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_files, container, false);
+        View view = inflater.inflate(R.layout.fragment_files, container, false);
+        // Find button and attach a click listener
+        files_notes_button = view.findViewById(R.id.files_notes_button);
+        files_notes_button.setOnClickListener(this::onNotesButtonClick);
+        return view;
+    }
+
+    private void onNotesButtonClick(View v) {
+        // go to NotesListActivity
+        Intent intent = new Intent(getActivity(), NotesListActivity.class);
+        startActivity(intent);
     }
 }
