1f71c1f1fa19945e6484b298de8036a3926d012e
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-01-14 14:49:49 +0100

    Initial commit (MainActivity + Fragments, RoomDB, material theme)

---
+++ app/src/main/java/com/veljkobogdan/quizzardapp/AppDatabase.java
@@ -0,0 +1,8 @@
+package com.veljkobogdan.quizzardapp;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+}
---
+++ app/src/main/java/com/veljkobogdan/quizzardapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.veljkobogdan.quizzardapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.veljkobogdan.quizzardapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.files) {
+                replaceFragment(new FilesFragment());
+            } else if (item.getItemId() == R.id.settings) {
+                replaceFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fTransaction = fm.beginTransaction();
+        fTransaction.replace(binding.frameLayout.getId(), fragment);
+        fTransaction.commit();
+    }
+}
