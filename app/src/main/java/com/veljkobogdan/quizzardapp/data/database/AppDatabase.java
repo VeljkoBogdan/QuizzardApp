3f4c7d30a7be5b32af33eb1fc2fbcbb5c8fe2501
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-01-18 16:58:27 +0100

    Very basic Note adding implemented

--- app/src/main/java/com/veljkobogdan/quizzardapp/data/database/AppDatabase.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/data/database/AppDatabase.java
@@ -11,7 +11,7 @@ import com.veljkobogdan.quizzardapp.data.database.entities.Note;
 
 @Database(entities = {
         Note.class
-}, version = 3)
+}, version = 4)
 public abstract class AppDatabase extends RoomDatabase {
     public static AppDatabase db;
     public static String name = "quizzard";
---
+++ app/src/main/java/com/veljkobogdan/quizzardapp/ui/notes/NewNoteActivity.java
@@ -0,0 +1,61 @@
+package com.veljkobogdan.quizzardapp.ui.notes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.veljkobogdan.quizzardapp.R;
+import com.veljkobogdan.quizzardapp.data.database.AppDatabase;
+import com.veljkobogdan.quizzardapp.data.database.entities.Note;
+import com.veljkobogdan.quizzardapp.data.repository.NoteRepository;
+import com.veljkobogdan.quizzardapp.databinding.ActivityNewNoteBinding;
+
+import java.time.LocalDateTime;
+
+public class NewNoteActivity extends AppCompatActivity {
+    ActivityNewNoteBinding binding;
+    NoteRepository noteRepository;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityNewNoteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        noteRepository = new NoteRepository(this);
+
+        Toolbar toolbar = binding.toolbarIncl.toolbar;
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("New Note"); // TODO: add a title in the action bar
+
+        binding.addButton.setOnClickListener(view -> {
+            String title = binding.title.getText().toString().trim();
+            String content = binding.content.getText().toString().trim();
+
+            if (title.isEmpty() || content.isEmpty()) {
+                return;
+            }
+
+            Note note = new Note();
+            note.setContent(content);
+            note.setTitle(title);
+            note.setCreatedAt(LocalDateTime.now().toString());
+
+            AppDatabase.getInstance(this).noteDao().insert(note);
+        });
+    }
+}
