3965d514f01c81337cd10561ec3d628b1b448153
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-01-24 16:33:23 +0100

    Notes fragment in Files fragment now shows notes

--- app/src/main/java/com/veljkobogdan/quizzardapp/ui/main/files/FilesNotesFragment.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/ui/main/files/FilesNotesFragment.java
@@ -1,60 +1,33 @@
 package com.veljkobogdan.quizzardapp.ui.main.files;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.veljkobogdan.quizzardapp.R;
+import com.veljkobogdan.quizzardapp.data.models.NoteWithTags;
+import com.veljkobogdan.quizzardapp.data.repository.NoteRepository;
+import com.veljkobogdan.quizzardapp.ui.notes.NoteAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FilesNotesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FilesNotesFragment extends Fragment {
+    private NoteRepository noteRepository;
+    private NoteAdapter noteAdapter;
+    private RecyclerView recyclerView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FilesNotesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FilesNotesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FilesNotesFragment newInstance(String param1, String param2) {
-        FilesNotesFragment fragment = new FilesNotesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public FilesNotesFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +35,42 @@ public class FilesNotesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_files_notes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = requireView().findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,
+                StaggeredGridLayoutManager.VERTICAL));
+
+        noteRepository = new NoteRepository(requireContext());
+        noteAdapter = new NoteAdapter(new NoteAdapter.OnNoteClickListener() {
+            @Override
+            public void onNoteClick(NoteWithTags note) {
+
+            }
+
+            @Override
+            public void onNoteLongClick(NoteWithTags note) {
+
+            }
+        });
+
+        recyclerView.setAdapter(noteAdapter);
+
+        loadNotes();
+    }
+
+    private void loadNotes() {
+        noteRepository.getAllNotesWithTags().observe(requireActivity(), notes -> {
+            if (!notes.isEmpty()) {
+                noteAdapter.setNotes(notes);
+                this.requireView().findViewById(R.id.noNotesText).setVisibility(View.GONE);
+            } else {
+                this.requireView().findViewById(R.id.noNotesText).setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
