4098e78422d08cc620460ef4e0db23362d410e67
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-03-05 13:52:58 +0100

    Implemented a simple Calendar View

--- app/src/main/java/com/veljkobogdan/quizzardapp/ui/main/CalendarFragment.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/ui/main/CalendarFragment.java
@@ -9,10 +9,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.kizitonwose.calendar.core.CalendarDay;
+import com.kizitonwose.calendar.view.CalendarView;
+import com.kizitonwose.calendar.view.MonthDayBinder;
+import com.kizitonwose.calendar.view.ViewContainer;
 import com.veljkobogdan.quizzardapp.R;
 
+import java.time.DayOfWeek;
+import java.time.YearMonth;
+import java.time.temporal.WeekFields;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class CalendarFragment extends Fragment {
+    private CalendarView calendarView;
 
     public CalendarFragment() {}
 
@@ -26,6 +38,27 @@ public class CalendarFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        calendarView = requireView().findViewById(R.id.calendarView);
+        calendarView.setDayBinder(new MonthDayBinder<DayViewContainer>() {
+            @Override
+            public void bind(@NonNull DayViewContainer container, CalendarDay calendarDay) {
+                container.calendarDayText.setText(String.valueOf(calendarDay.getDate().getDayOfMonth()));
+            }
+
+            @NonNull
+            @Override
+            public DayViewContainer create(@NonNull View view) {
+                return new DayViewContainer(view);
+            }
+
+        });
+
+        YearMonth currentMonth = YearMonth.now();
+        YearMonth startMonth = currentMonth.minusMonths(1);
+        YearMonth endMonth = currentMonth.plusMonths(1);
+        DayOfWeek firstDayOfWeek = WeekFields.of(Locale.getDefault()).getFirstDayOfWeek();
+        calendarView.setup(startMonth, endMonth, firstDayOfWeek);
+        calendarView.scrollToMonth(currentMonth);
     }
 
     @Override
@@ -33,5 +66,14 @@ public class CalendarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+
+    class DayViewContainer extends ViewContainer {
+        TextView calendarDayText;
+
+        public DayViewContainer(View view) {
+            super(view);
+            calendarDayText = view.findViewById(R.id.calendarDayText);
+        }
     }
 }
