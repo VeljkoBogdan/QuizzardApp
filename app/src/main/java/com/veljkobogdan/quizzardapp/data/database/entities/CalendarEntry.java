a2ad7f118baaa1536b3cacfc61dc0725bfb18be0
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-03-10 12:54:54 +0100

    Added a title to calendar entry, added a nullable notation to localDateTime

--- app/src/main/java/com/veljkobogdan/quizzardapp/data/database/entities/CalendarEntry.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/data/database/entities/CalendarEntry.java
@@ -1,5 +1,6 @@
 package com.veljkobogdan.quizzardapp.data.database.entities;
 
+import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Ignore;
@@ -14,16 +15,20 @@ public class CalendarEntry {
     public long calendarId;
 
     @ColumnInfo
+    public String title;
+    @ColumnInfo
     // Required
     public LocalDate localDate;
     @ColumnInfo
+    @Nullable
     // Optional
     public LocalDateTime localDateTime;
 
     public CalendarEntry() {}
 
     @Ignore
-    public CalendarEntry(LocalDate localDate, LocalDateTime localDateTime) {
+    public CalendarEntry(String title, LocalDate localDate, @Nullable LocalDateTime localDateTime) {
+        this.title = title;
         this.localDate = localDate;
         this.localDateTime = localDateTime;
     }
