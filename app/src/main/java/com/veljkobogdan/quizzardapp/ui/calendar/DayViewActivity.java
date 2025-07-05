15e46ca98812a4ccee2689c1d20096cb30fb2159
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-07-05 14:16:50 +0200

    Events now show in Calendar Day Vie Activity

--- app/src/main/java/com/veljkobogdan/quizzardapp/ui/calendar/DayViewActivity.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/ui/calendar/DayViewActivity.java
@@ -1,8 +1,11 @@
 package com.veljkobogdan.quizzardapp.ui.calendar;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,16 +17,22 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.button.MaterialButton;
 import com.kizitonwose.calendar.core.CalendarDay;
 import com.veljkobogdan.quizzardapp.R;
+import com.veljkobogdan.quizzardapp.data.database.entities.CalendarEntry;
+import com.veljkobogdan.quizzardapp.data.repository.CalendarEntryRepository;
 import com.veljkobogdan.quizzardapp.databinding.ActivityDayViewBinding;
 import com.veljkobogdan.quizzardapp.util.IntentGroup;
 
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DayViewActivity extends AppCompatActivity {
     private ActivityDayViewBinding binding;
     private CalendarDay day;
     private MaterialButton addEventButton;
     private LinearLayout calendarEntryLayout;
+    private List<CalendarEntry> entries = new ArrayList<>();
+    private CalendarEntryRepository calendarEntryRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +54,53 @@ public class DayViewActivity extends AppCompatActivity {
         toolbar.setTitle(day.getDate().format(DateTimeFormatter.ofPattern("MMMM d yyyy")));
         setSupportActionBar(toolbar);
 
+        calendarEntryRepository = new CalendarEntryRepository(this);
+
         this.addEventButton = binding.addEventButton;
         this.calendarEntryLayout = binding.calendarEntryLayout;
+
+        this.addEventButton.setOnClickListener(view -> {
+            // TODO: Open a new activity to create an entry
+        });
+
+        getEntries();
+        populateEntryLayout();
     }
 
     private void getIntentContent() {
         this.day = (CalendarDay) getIntent().getSerializableExtra(IntentGroup.DAY);
+    }
+
+    private void getEntries() {
+        calendarEntryRepository.getCalendarEntriesForDay(day).observe(this, updatedEntries -> {
+            if (!updatedEntries.isEmpty()) {
+                entries = updatedEntries;
+            }
+        });
+    }
+
+    private void populateEntryLayout() {
+        TextView eventTitle, eventTime;
+
+        for (CalendarEntry calendarEntry : entries) {
+            View card = LayoutInflater.from(this).inflate(R.layout.item_event, binding.getRoot(), false);
+            eventTitle = card.findViewById(R.id.eventName);
+            eventTime = card.findViewById(R.id.eventTime);
+
+            eventTitle.setText(calendarEntry.title);
+
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d MMM, yyyy 'at' HH:mm");
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d MMM, yyyy");
+
+            String formattedTime;
+            if (calendarEntry.localDateTime != null) {
+                formattedTime = calendarEntry.localDateTime.format(dateTimeFormatter);
+            } else {
+                formattedTime = calendarEntry.localDate.format(dateFormatter);
+            }
+            eventTime.setText(formattedTime);
+
+            calendarEntryLayout.addView(card);
+        }
     }
 }
