08a453ef642b8d8c75249b0b4fa3b4b55f90a9af
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-07-14 17:22:14 +0200

    Schedule layout basics and connected to main activity

--- app/src/main/java/com/veljkobogdan/quizzardapp/ui/main/MainActivity.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/ui/main/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
             } else if (item.getItemId() == R.id.calendar) {
                 replaceFragment(new CalendarFragment());
             } else if (item.getItemId() == R.id.schedule) {
-                // TODO: Add Schedule Fragment
+                replaceFragment(new ScheduleFragment());
             }
 
             return true;
---
+++ app/src/main/java/com/veljkobogdan/quizzardapp/ui/main/ScheduleFragment.java
@@ -0,0 +1,37 @@
+package com.veljkobogdan.quizzardapp.ui.main;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.veljkobogdan.quizzardapp.R;
+
+
+public class ScheduleFragment extends Fragment {
+
+    public ScheduleFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
