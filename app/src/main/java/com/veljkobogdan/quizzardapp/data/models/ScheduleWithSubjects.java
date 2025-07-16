820457b8162f54cafffad89e0e131efdfc216850
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-07-16 16:37:52 +0200

    Schedule and Subjects junction fix

--- app/src/main/java/com/veljkobogdan/quizzardapp/data/models/ScheduleWithSubjects.java
+++ app/src/main/java/com/veljkobogdan/quizzardapp/data/models/ScheduleWithSubjects.java
@@ -2,9 +2,12 @@ package com.veljkobogdan.quizzardapp.data.models;
 
 import androidx.room.Embedded;
 import androidx.room.Ignore;
+import androidx.room.Junction;
 import androidx.room.Relation;
 
+import com.veljkobogdan.quizzardapp.data.database.entities.NoteTagCrossRef;
 import com.veljkobogdan.quizzardapp.data.database.entities.Schedule;
+import com.veljkobogdan.quizzardapp.data.database.entities.ScheduleWithSubjectsCrossRef;
 import com.veljkobogdan.quizzardapp.data.database.entities.Subject;
 
 import java.util.List;
@@ -15,7 +18,8 @@ public class ScheduleWithSubjects {
 
     @Relation(
             parentColumn = "scheduleId",
-            entityColumn = "subjectId"
+            entityColumn = "subjectId",
+            associateBy = @Junction(ScheduleWithSubjectsCrossRef.class)
     )
     public List<Subject> subjectList;
 
